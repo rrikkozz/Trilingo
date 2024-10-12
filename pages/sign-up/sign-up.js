dfaddeafd9de98da69739b7dc18073a62483bde3
Mouad Ghouti <mouath.games502@gmail.com>
2024-10-12 22:21:12 +0300

    created login and sign-up pages

---
+++ pages/sign-up/sign-up.js
@@ -0,0 +1,23 @@
+let currentTestimonial = 0;
+const testimonials = document.querySelectorAll('.testimonial-item');
+
+function showTestimonial(index) {
+    testimonials.forEach((testimonial, i) => {
+        testimonial.classList.remove('active');
+        if (i === index) {
+            testimonial.classList.add('active');
+        }
+    });
+}
+
+function nextTestimonial() {
+    currentTestimonial = (currentTestimonial + 1) % testimonials.length;
+    showTestimonial(currentTestimonial);
+}
+
+function prevTestimonial() {
+    currentTestimonial = (currentTestimonial - 1 + testimonials.length) % testimonials.length;
+    showTestimonial(currentTestimonial);
+}
+
+showTestimonial(currentTestimonial);
